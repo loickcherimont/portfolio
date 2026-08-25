eb2208575ef6f7aabb92ef13ba140cc40e3d4bd4
loickcherimont <loickcherimont@gmail.com>
2026-08-25 11:31:18 +0200

    feat: add about section to the portfolio

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,10 +4,11 @@ import { Hero } from '../hero/hero';
 import { Realisations } from '../realisations/realisations';
 import { Stack } from '../stack/stack';
 import { Experiences } from '../experiences/experiences';
+import { About } from "../about/about";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Realisations, Stack, Experiences],
+  imports: [Hero, Realisations, Stack, Experiences, About],
   templateUrl: './home.html',
 })
 export class Home {
